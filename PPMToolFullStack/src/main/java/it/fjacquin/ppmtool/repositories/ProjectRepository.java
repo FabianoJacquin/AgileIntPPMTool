6e1a2f195ffd796d3e5b4aa101fd9ca2c8d3c21d
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-25 13:07:11 +0200

    Implemented Project object and created Project repository

---
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package it.fjacquin.ppmtool.repositories;
+
+import it.fjacquin.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
