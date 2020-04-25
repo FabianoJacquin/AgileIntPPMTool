7e0094acb9afab28c6a8a5bca69d460376fb778e
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-04-25 20:08:17 +0200

    Created custom exception for projectIdentifier (UNIQUE)

--- PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package it.fjacquin.ppmtool.services;
 
 import it.fjacquin.ppmtool.domain.Project;
+import it.fjacquin.ppmtool.exceptions.ProjectIdException;
 import it.fjacquin.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,16 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
      public Project saveOrUpdateProject(Project project){
-         return  projectRepository.save(project);
+
+         try{
+             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+             return  projectRepository.save(project);
+         } catch (Exception e){
+             throw new ProjectIdException("Project ID '" +
+                     project.getProjectIdentifier().toUpperCase() + "'already exist");
+         }
+
+
      }
 
 }
