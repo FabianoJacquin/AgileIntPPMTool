9aa801d5d0b08c62b2476a977afb2a267974ea55
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-05-14 06:40:57 +0200

    Backlog and ProjectTask entities and repositories

---
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package it.fjacquin.ppmtool.repositories;
+
+import it.fjacquin.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
---
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package it.fjacquin.ppmtool.repositories;
+
+import it.fjacquin.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
