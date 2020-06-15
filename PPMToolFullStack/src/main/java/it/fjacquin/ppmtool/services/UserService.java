20245b737106590937f30295dfd6897189414b1d
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-06-15 06:28:29 +0200

    Create User Object, Validation, Repository and Service

---
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/services/UserService.java
@@ -0,0 +1,13 @@
+package it.fjacquin.ppmtool.services;
+
+import it.fjacquin.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+}
