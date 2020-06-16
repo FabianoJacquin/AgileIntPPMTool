27a38beb58f092332d4308503b5dfc8a97ed7e3a
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-06-16 06:23:31 +0200

    User Registration part 1

--- PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/it/fjacquin/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package it.fjacquin.ppmtool.services;
 
+import it.fjacquin.ppmtool.domain.User;
 import it.fjacquin.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,21 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser (User newUser){
+
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        //Username has to be unique (exception)
+        
+        //Make sure that password and confirm Password match
+        //We don't persist or show confirmPassword
+
+        return userRepository.save(newUser);
+
+    }
 
 }
