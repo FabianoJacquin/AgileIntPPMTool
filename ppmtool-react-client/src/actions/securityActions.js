b0fe32494d76e8c74d96a44c772904a7d3e15e7f
Fabiano Jacquin <fabiano.jacquin@gmail.com>
2020-07-07 06:46:43 +0200

    Login Form

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -19,7 +19,7 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
   }
 };
 
-export const Login = (LoginRequest) => async (dispatch) => {
+export const login = (LoginRequest) => async (dispatch) => {
   try {
     // post LoginRequest
     const res = await axios.post("/api/users/login", LoginRequest);
